033073ac586749d4bf292cc4241aa020604c1754
Rmohandigital <rmohankiran74@gmail.com>
2026-08-05 18:16:25 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,24 @@
+// Import Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-firestore.js";
+
+// Firebase Configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDarA4FEnJ0Sg7cDxf8uPglDmrfZ6lgJ9I",
+  authDomain: "r-mohan-digital-e4e4b.firebaseapp.com",
+  projectId: "r-mohan-digital-e4e4b",
+  storageBucket: "r-mohan-digital-e4e4b.firebasestorage.app",
+  messagingSenderId: "56539770062",
+  appId: "1:56539770062:web:0c815083264f74a189349f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Services
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+// Export
+export { auth, db };
