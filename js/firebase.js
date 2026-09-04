3437e59508cf9f75e91a6552d4cfd3801e57ee4c
Rmohandigital <rmohankiran74@gmail.com>
2026-09-04 22:12:39 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -11,31 +11,17 @@ import { getAuth } from
 import { getFirestore } from
     "https://www.gstatic.com/firebasejs/12.17.1/firebase-firestore.js";
 
-
-// ==========================================
-// FIREBASE CONFIG
-// ==========================================
+import { getStorage } from
+    "https://www.gstatic.com/firebasejs/12.17.1/firebase-storage.js";
 
 const firebaseConfig = {
-
-    apiKey: "AIzaSyDarA4FEnJ0Sg7cDxf8uPglDmrfZ6lgJ9I",
-
+    apiKey: "YOUR_API_KEY",
     authDomain: "r-mohan-digital-e4e4b.firebaseapp.com",
-
     projectId: "r-mohan-digital-e4e4b",
-
     storageBucket: "r-mohan-digital-e4e4b.firebasestorage.app",
-
     messagingSenderId: "56539770062",
-
     appId: "1:56539770062:web:0c815083264f74a189349f"
-
 };
-
-
-// ==========================================
-// INITIALIZE FIREBASE
-// ==========================================
 
 const app = initializeApp(firebaseConfig);
 
@@ -43,13 +29,11 @@ const auth = getAuth(app);
 
 const db = getFirestore(app);
 
-
-// ==========================================
-// EXPORT
-// ==========================================
+const storage = getStorage(app);
 
 export {
     app,
     auth,
-    db
+    db,
+    storage
 };
