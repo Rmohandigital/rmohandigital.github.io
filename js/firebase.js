5f9c732e554710a14300f3d7f39436de08730fd6
Rmohandigital <rmohankiran74@gmail.com>
2026-09-05 17:42:32 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,6 +1,4 @@
-// ============================================================
 // R MOHAN DIGITAL - FIREBASE CONNECTION
-// ============================================================
 
 import {
     initializeApp
@@ -21,42 +19,27 @@ import {
 
 const firebaseConfig = {
 
-    apiKey:
-        "AIzaSyDarA4FEnJ0Sg7cDxf8uPglDmrfZlgJ9I",
+    apiKey: "AIzaSyDarA4FEnJ0Sg7cDxf8uPglDmrfZlg9J9I",
 
-    authDomain:
-        "r-mohan-digital-e4e4b.firebaseapp.com",
+    authDomain: "r-mohan-digital-e4e4b.firebaseapp.com",
 
-    projectId:
-        "r-mohan-digital-e4e4b",
+    projectId: "r-mohan-digital-e4e4b",
 
-    storageBucket:
-        "r-mohan-digital-e4e4b.firebasestorage.app",
+    storageBucket: "r-mohan-digital-e4e4b.firebasestorage.app",
 
-    messagingSenderId:
-        "56539770062",
+    messagingSenderId: "56539770062",
 
-    appId:
-        "1:56539770062:web:0c815083264f74a189349f"
+    appId: "1:56539770062:web:0c815083264f74a189349f"
 };
 
 
-const app =
-    initializeApp(
-        firebaseConfig
-    );
+const app = initializeApp(firebaseConfig);
 
+const auth = getAuth(app);
 
-const auth =
-    getAuth(app);
+const db = getFirestore(app);
 
-
-const db =
-    getFirestore(app);
-
-
-const storage =
-    getStorage(app);
+const storage = getStorage(app);
 
 
 export {
